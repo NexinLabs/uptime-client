a418168c28c695d03f9feb84f93abb18f41b165a
hunter87ff <huntergaming702@gmail.com>
2025-10-16 00:14:21 +0530

    feat: Add Dockerfile and .dockerignore for containerization setup; define multi-stage build process

--- server/src/routes/endpoints.ts
+++ server/src/routes/endpoints.ts
@@ -4,4 +4,9 @@ const endpoints = Router();
 
 endpoints.use("/auth", authRouter);
 
+
+
+endpoints.get("/", (req, res) => {
+    res.json({ message: "API is running" });
+});
 export default endpoints;
